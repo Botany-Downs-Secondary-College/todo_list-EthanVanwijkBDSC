834af3865e09ba6ddc6526b57936f93eec405802
EthanVanwijkBDSC <78832211+EthanVanwijkBDSC@users.noreply.github.com>
2021-02-16 11:56:18 +1300

    Update list.py

--- list.py
+++ list.py
@@ -7,14 +7,14 @@ Created on Tue Feb 16 11:19:45 2021
 question = ""
 list = []
 stage = 1
-# defines variables and listsw
+# defines variables and lists
 
 while stage == 1:
     num = int(input("choose a mode by entering a number:\n"" 1: add task\n"" 2: View list\n"" 3: exit \n""  :"))
     if num == 1:
         while question != "end":
             question = input("add items to the list by typeing them and type end to stop adding items: ")
-        list.append(question)
+            list.append(question)
     elif num == 2:
         print(list)
     elif num == 3:
