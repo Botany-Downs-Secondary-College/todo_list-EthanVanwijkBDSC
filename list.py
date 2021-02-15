00fdf82fb774b26ba67a5c4ae018d1436d0c7aa7
EthanVanwijkBDSC <78832211+EthanVanwijkBDSC@users.noreply.github.com>
2021-02-16 11:40:23 +1300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 11:19:45 2021
+
+@author: Ethan Van Wijk
+"""
+list = []
+stage = 1
+# defines variables and listsw
+
+while stage == 1:
+    num = int(input("choose a mode by entering a number:\n"" 1: add task\n"" 2: View list\n"" 3: exit \n""  :"))
+    if num == 1:
+        while question != "end":
+            uestion = input("add items to the list by typeing them and type end to stop adding items: ")
+            print("n/")
+        else:
+            list.append(question)
+    elif num == 2:
+        print(list)
+    elif num == 3:
+        stage = 2
+    else:
+        print("that not a valid number")
+# asks for list items
+
+print(list)
+print("the list is complete")
+
+#ends code
